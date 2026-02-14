fd5e571601ad092a5306f2c5c011e440272c7f39
miztli melgoza <miztli.melgoza@sephora.com>
2026-02-14 15:46:50 -0600

    Initial Commit

---
+++ GreetingApp/GreetingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreetingAppApp.swift
+//  GreetingApp
+//
+//  Created by Miztli Eduardo Melgoza Hernandez on 14/02/26.
+//
+
+import SwiftUI
+
+@main
+struct GreetingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GreetingAppTests/GreetingAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  GreetingAppTests.swift
+//  GreetingAppTests
+//
+//  Created by Miztli Eduardo Melgoza Hernandez on 14/02/26.
+//
+
+import Testing
+@testable import GreetingApp
+
+struct GreetingAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
